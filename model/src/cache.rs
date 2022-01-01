aebd3cd8d196fb9ab99968b2b8b5f9025cb99106
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-01-01 14:10:50 +0100

    feat(model): cached channel models

--- model/src/cache.rs
+++ model/src/cache.rs
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 use twilight_model::{
-    channel::{permission_overwrite::PermissionOverwrite, ChannelType},
+    channel::permission_overwrite::PermissionOverwrite,
     datetime::Timestamp,
     guild::Permissions,
     id::{ChannelId, GuildId, RoleId, UserId},
@@ -20,22 +20,22 @@ use twilight_model::{
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct CachedGuild {
     /// Id of the guild.
-    id: GuildId,
+    pub id: GuildId,
     /// Name of the guild
-    name: String,
+    pub name: String,
     /// Hash of the guild icon.
-    icon: Option<String>,
+    pub icon: Option<String>,
     /// Id of the guild's owner.
-    owner_id: UserId,
+    pub owner_id: UserId,
     /// Information about the bot member in the guild.
     ///
     /// If this field is [`None`], the information has not been
     /// properly received and all permission calculations should fail.
-    current_member: Option<CurrentMember>,
+    pub current_member: Option<CurrentMember>,
     /// List of roles of the guild.
-    roles: HashMap<RoleId, PartialRole>,
+    pub roles: HashMap<RoleId, PartialRole>,
     /// List of channels of the guild.
-    channels: HashMap<ChannelId, PartialChannel>,
+    pub channels: HashMap<ChannelId, PartialChannel>,
 }
 
 /// Information about the bot [`Member`] in a guild.
@@ -44,14 +44,47 @@ pub struct CachedGuild {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct CurrentMember {
     /// Id of the bot current member.
-    id: UserId,
+    pub id: UserId,
     /// When the bot can resume communication in a guild again.
     ///
     /// Checking if this value is [`Some`] is not enough, we should
     /// also check that the given timestamp is not in the past.
-    communication_disabled_until: Option<Timestamp>,
+    pub communication_disabled_until: Option<Timestamp>,
     /// Roles of the bot.
-    roles: Vec<RoleId>,
+    pub roles: Vec<RoleId>,
+}
+
+/// Cached model of a [`Role`].
+///
+/// This model is not cached within guilds to limit
+/// data to send when requesting a [`CachedGuild`].
+///
+/// [`Role`]: twilight_model::guild::Role
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct CachedRole {
+    /// Id of the role.
+    pub id: RoleId,
+    /// Id of the guild to which the role belongs.
+    pub guild_id: GuildId,
+    /// Name of the role.
+    pub name: String,
+    /// Color of the role.
+    pub color: u32,
+    /// Icon image hash.
+    pub icon: Option<String>,
+    /// Icon unicode emoji.
+    ///
+    /// This field is set if the role has an icon which is
+    /// not a custom image but an existing unicode emoji.
+    pub unicode_emoji: Option<String>,
+    /// Position of the role.
+    pub position: i64,
+    /// Permissions of the role.
+    pub permissions: Permissions,
+    /// Whether the role is managed.
+    ///
+    /// Managed roles include bot, integration or boost roles.
+    pub managed: bool,
 }
 
 /// Partial model of a [`Role`].
@@ -67,51 +100,133 @@ pub struct PartialRole {
     /// The position *should* be positive but can be negative
     /// in some cases. Only the ordering is important for
     /// permission calculations.
-    position: i64,
+    pub position: i64,
     /// Permissions of the role.
-    permissions: Permissions,
+    pub permissions: Permissions,
 }
 
-/// Partial model of a guild [`Channel`].
+/// Cached model of a [`GuildChannel`].
 ///
-/// This type is used in [`CachedGuild`] and only contains fields
-/// required for permissions calculation.
+/// Only text channels and threads are cached as the bot
+/// does not interact with voice channels.
 ///
-/// [`Channel`]: twilight_model::channel::Channel
+/// [`GuildChannel`]: twilight_model::channel::GuildChannel
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct PartialChannel {
-    /// Type of channel (text, category, voice, ...)
-    kind: ChannelType,
-    permission_overwrites: Vec<PermissionOverwrite>,
+pub enum CachedChannel {
+    /// Text channel.
+    Text(CachedTextChannel),
+    /// Category channel.
+    Category(CachedCategoryChannel),
+    /// Public or private thread.
+    Thread(CachedThread),
 }
 
-/// Cached model of a [`Role`].
+/// Cached model of a [`TextChannel`].
 ///
-/// This model is not cached within guilds to limit
-/// data to send when requesting a [`CachedGuild`].
-///
-/// [`Role`]: twilight_model::guild::Role
+/// [`TextChannel`]: twilight_model::channel::TextChannel
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct CachedRole {
-    /// Id of the role.
-    id: RoleId,
-    /// Name of the role.
-    name: String,
-    /// Color of the role.
-    color: u32,
-    /// Icon image hash.
-    icon: Option<String>,
-    /// Icon unicode emoji.
+pub struct CachedTextChannel {
+    /// Id of the channel.
+    pub id: ChannelId,
+    /// Id of the guild to which the channel belongs.
+    pub guild_id: GuildId,
+    /// Name of the channel.
+    pub name: String,
+    /// If the channel is in a category, the category id.
+    pub parent_id: Option<ChannelId>,
+    /// Sorting position of the channel.
+    pub position: i64,
+    /// Permission overwrites of the channel.
+    pub permission_overwrites: Vec<PermissionOverwrite>,
+    /// Amount of seconds a user has to wait between two message.
+    pub rate_limit_per_user: Option<u64>,
+}
+
+/// Cached model of a [`CategoryChannel`].
+///
+/// [`CategoryChannel`]: twilight_model::channel::CategoryChannel
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct CachedCategoryChannel {
+    /// Id of the category.
+    pub id: ChannelId,
+    /// Id of the guild to which the category belongs.
+    pub guild_id: GuildId,
+    /// Name of the category.
+    pub name: String,
+    /// Sorting position of the category.
+    pub position: i64,
+    /// Permission overwrites of the category.
+    pub permission_overwrites: Vec<PermissionOverwrite>,
+}
+
+/// Cached model of a [`PublicThread`] or [`PrivateThread`].
+///
+/// The bot does not distinguish between private and public threads during processing.
+///
+/// [`PublicThread`]: twilight_model::channel::thread::PublicThread
+/// [`PrivateThread`]: twilight_model::channel::thread::PrivateThread
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct CachedThread {
+    /// Id of the thread.
+    pub id: ChannelId,
+    /// Id of the guild to which the thread belongs.
+    pub guild_id: GuildId,
+    /// Name of the thread.
+    pub name: String,
+    /// Whether the thread is private.
+    pub private: bool,
+    /// Parent channel of the thread.
     ///
-    /// This field is set if the role has an icon which is
-    /// not a custom image but an existing unicode emoji.
-    unicode_emoji: Option<String>,
-    /// Position of the role.
-    position: i64,
-    /// Permissions of the role.
-    permissions: Permissions,
-    /// Whether the role is managed.
-    ///
-    /// Managed roles include bot, integration or boost roles.
-    managed: bool,
+    /// This field can be [`None`] if the parent channel has been
+    /// deleted.
+    pub parent_id: Option<ChannelId>,
+    /// Amount of seconds a user has to wait between two message.
+    pub rate_limit_per_user: Option<u64>,
+}
+
+/// Partial model of a [`GuildChannel`].
+///
+/// This type is used in [`CachedGuild`] and only contain fields
+/// required for permissions calculation. Only text channels and threads
+/// are cached as the bot does not interact with voice channels.
+///
+/// [`GuildChannel`]: twilight_model::channel::GuildChannel
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub enum PartialChannel {
+    /// Partial text channel.
+    Text(PartialTextChannel),
+    /// Partial category channel.
+    Category(PartialCategoryChannel),
+    /// Partial thread.
+    Thread(PartialThread),
+}
+
+/// Partial model of a [`TextChannel`].
+///
+/// [`TextChannel`]: twilight_model::channel::TextChannel
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct PartialTextChannel {
+    /// If the channel is in a category, the category id.
+    pub parent_id: Option<ChannelId>,
+    /// Permission overwrites of the channel.
+    pub permission_overwrites: Vec<PermissionOverwrite>,
+}
+
+/// Partial model of a [`CategoryChannel`].
+///
+/// [`CategoryChannel`]: twilight_model::channel::CategoryChannel
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct PartialCategoryChannel {
+    /// Permission overwrites of the channel.
+    pub permission_overwrites: Vec<PermissionOverwrite>,
+}
+
+/// Cached model of a [`PublicThread`] or [`PrivateThread`].
+///
+/// [`PublicThread`]: twilight_model::channel::thread::PublicThread
+/// [`PrivateThread`]: twilight_model::channel::thread::PrivateThread
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct PartialThread {
+    /// Parent channel of the thread.
+    pub parent_id: Option<ChannelId>,
 }
