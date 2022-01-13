223dc7d6a7583fc19ad32aba1c2ccef527075eaf
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-01-13 08:34:17 +0000

    feat: add guild events

---
+++ model/src/event.rs
@@ -0,0 +1,52 @@
+//! Event models.
+//!
+//! This module contains models representing Discord events received by theg
+//! gateway. They match [`twilight_model::gateway::event`] models with additional
+//! fields for cached ressources.
+
+use serde::{Deserialize, Serialize};
+use twilight_model::{
+    guild::{Guild, PartialGuild},
+    id::GuildId,
+};
+
+use crate::cache::CachedGuild;
+
+/// Event received from Discord.
+///
+/// This type contain all events that can be received from Discord and handled
+/// by the bot. Only event types that are processed are included.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum Event {
+    GuildJoin(GuildJoin),
+    GuildUpdate(GuildUpdate),
+    GuildLeave(GuildLeave),
+}
+
+/// Guild create event.
+///
+/// This event is only emitted after the shard is ready to avoid sending events
+/// while filling the cache.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct GuildJoin {
+    pub guild: Guild,
+}
+
+/// Guild update event.
+///
+/// The event is not emitted if the guild was not previously cached.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct GuildUpdate {
+    pub cached: CachedGuild,
+    pub guild: PartialGuild,
+}
+
+/// Guild delete event.
+///
+/// This event is not emitted if the guild was deleted because it becomes
+/// unavaiable.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct GuildLeave {
+    pub cached: Option<CachedGuild>,
+    pub id: GuildId,
+}
