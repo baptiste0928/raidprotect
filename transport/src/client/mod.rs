8559505572f9d264b2e3be72ae4f005a0b4203e4
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-02-18 14:23:47 +0000

    fix documentation links

--- transport/src/client/mod.rs
+++ transport/src/client/mod.rs
@@ -1,13 +1,10 @@
 //! Gateway client.
 //!
-//! This module contain the client corresponding to the server of the [`server`]
-//! module of this crate. See the server documentation for details about the
-//! protocol.
+//! This module contain the client for the server exposed by the gateway. See*
+//! the server documentation for details about the protocol.
 //!
 //! The inner client state is wrapped in an `RwLock<Arc>` and can be cloned
 //! cheaply. Reconnection logic is handled internally.
-//!
-//! [`server`]: crate::server
 #![allow(unused)]
 
 #[allow(clippy::module_inception)]
