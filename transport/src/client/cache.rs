ccd9dbedafd30c90a587ff28ffffb49f424b3943
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-02-21 19:36:47 +0000

    refactor: delete transport and model crates

--- transport/src/client/cache.rs
+++
@@ -1,8 +0,0 @@
-/// Cache client with automatic reconnection.
-///
-/// This client wraps [`cache::CacheClient`] with automatic reconnection to the
-/// server.
-///
-/// [`cache::CacheClient`]: crate::cache::CacheClient
-#[derive(Debug)]
-pub struct CacheClient {}
