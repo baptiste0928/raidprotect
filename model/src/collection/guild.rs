9b6dfd004653473e5eada5d3454ed739708f83b6
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-04-11 19:10:23 +0200

    Refactor helpers using serde_with

--- model/src/collection/guild.rs
+++ model/src/collection/guild.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
 use twilight_model::id::{
     marker::{ChannelMarker, GuildMarker, RoleMarker},
     Id,
 };
 
-use super::serde_helpers::{id_as_i64, option_id_as_i64};
+use super::serde_helpers::IdAsI64;
 
 /// Name of the `guilds` collection
 pub const GUILDS_COLLECTION: &str = "guilds";
@@ -12,16 +13,19 @@ pub const GUILDS_COLLECTION: &str = "guilds";
 /// Guild data and configuration.
 ///
 /// This struct correspond to documents stored in the `guilds` collection.
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Guild {
     /// Discord guild id.
-    #[serde(rename = "_id", with = "id_as_i64")]
+    #[serde_as(as = "IdAsI64")]
+    #[serde(rename = "_id")]
     pub id: Id<GuildMarker>,
 }
 
 /// A guild configuration.
 ///
 /// Stored in the `guilds` collection within a [`Guild`].
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(default)]
 pub struct Config {
@@ -30,7 +34,7 @@ pub struct Config {
     /// The configuration validation will fail if no logs chan is set,
     /// but this field may be [`None`] when the initial configuration
     /// has not yet be done.
-    #[serde(with = "option_id_as_i64")]
+    #[serde_as(as = "Option<IdAsI64>")]
     pub logs_chan: Option<Id<ChannelMarker>>,
     /// The moderator roles, allowed to access to guild modlogs
     /// and to perform some sanction (mute and warn).
--- model/src/collection/serde_helpers.rs
+++ model/src/collection/serde_helpers.rs
@@ -1,26 +1,34 @@
 //! Serde helpers used in the `collection` module.
 //!
-//! These modules are intended to be used with `#[serde(with = "")]` and
-//! customize the serialization and deserialization behavior of the fields
-//! they are applied on.
+//! These modules are intended to be used with [`serde_as`] and customize the
+//! serialization and deserialization behavior of the fields they are applied on.
+//!
+//! [`serde_as`]: serde_with::serde_as
 
-pub mod id_as_i64 {
-    //! Serialize twilight [`Id`] as [`i64`].
-    //!
-    //! Ids are serialized as [`i64`] because BSON (used by MongoDB) doesn't
-    //! support storing integers as [`u64`].
-    //!
-    //! Because one bit is lost when using [`i64`], the maximum timestamp
-    //! that can be stored is Sep 06 2084 (which shouldn't be a problem). Any
-    //! id that does not fit in a [`i64`] will produce an error.
+use std::num::NonZeroI64;
 
-    use std::num::NonZeroI64;
+use serde::{ser::Error, Deserialize, Deserializer, Serializer};
+use serde_with::{DeserializeAs, SerializeAs};
+use twilight_model::id::Id;
 
-    use serde::{ser, Deserialize, Deserializer, Serializer};
-    use twilight_model::id::Id;
+/// Serialize twilight [`Id`] as [`i64`].
+///
+/// This type implement [`SerializeAs`] and [`DeserializeAs`] and should be
+/// used with the [`serde_as`] macro.
+///
+/// Ids are serialized as [`i64`] because BSON (used by MongoDB) doesn't
+/// support storing integers as [`u64`].
+///
+/// Because one bit is lost when using [`i64`], the maximum timestamp
+/// that can be stored is Sep 06 2084 (which shouldn't be a problem). Any
+/// id that does not fit in a [`i64`] will produce an error.
+///
+/// [`serde_as`]: serde_with::serde_as
+#[derive(Debug, Clone, Copy)]
+pub struct IdAsI64;
 
-    /// Attempts to deserialize an [`Id`] from an [`i64`].
-    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Id<T>, D::Error>
+impl<'de, T> DeserializeAs<'de, Id<T>> for IdAsI64 {
+    fn deserialize_as<D>(deserializer: D) -> Result<Id<T>, D::Error>
     where
         D: Deserializer<'de>,
     {
@@ -28,51 +36,16 @@ pub mod id_as_i64 {
 
         Ok(Id::new(id as u64))
     }
-
-    /// Attempts to serialize an [`Id`] to an [`i64`].
-    pub fn serialize<S, T>(val: &Id<T>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-        T: Copy,
-    {
-        match i64::try_from(val.get()) {
-            Ok(val) => serializer.serialize_i64(val),
-            Err(_) => Err(ser::Error::custom(format!("cannot convert {} to i64", val))),
-        }
-    }
 }
 
-pub mod option_id_as_i64 {
-    //! Serialize twilight [`Option<Id>`] as [`Option<i64>`].
-    //!
-    //! This module is the same as [`id_as_i64`] but wraps type in [`Option`].
-
-    use std::num::NonZeroI64;
-
-    use serde::{Deserialize, Deserializer, Serializer};
-    use twilight_model::id::Id;
-
-    use super::id_as_i64;
-
-    /// Attempts to deserialize an [`Id`] from an [`i64`].
-    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Option<Id<T>>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let id = Option::<NonZeroI64>::deserialize(deserializer)?;
-
-        Ok(id.map(|id| Id::new(id.get() as u64)))
-    }
-
-    /// Attempts to serialize an [`Id`] to an [`i64`].
-    pub fn serialize<S, T>(val: &Option<Id<T>>, serializer: S) -> Result<S::Ok, S::Error>
+impl<T: Copy> SerializeAs<Id<T>> for IdAsI64 {
+    fn serialize_as<S>(source: &Id<T>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
-        T: Copy,
     {
-        match val {
-            Some(val) => id_as_i64::serialize(val, serializer),
-            None => serializer.serialize_none(),
+        match i64::try_from(source.get()) {
+            Ok(val) => serializer.serialize_i64(val),
+            Err(_) => Err(Error::custom(format!("cannot convert {} to i64", source))),
         }
     }
 }
