06c53c9e43e514da2f3c07d8b9b22325c9e32389
Hans Tognon <learningway403@gmail.com>
2022-07-30 15:04:14 +0100

    feat: informative message for old commands

--- raidprotect/src/event/message/handle.rs
+++ raidprotect/src/event/message/handle.rs
@@ -1,10 +1,17 @@
 use std::sync::Arc;
 
-use tracing::info;
+use anyhow::{Context, Result};
+use rosetta_i18n::Language;
+use tracing::{error, info};
+use twilight_http::Client;
 use twilight_model::channel::Message;
+use twilight_util::builder::embed::EmbedBuilder;
+
+use crate::cluster::ClusterState;
+use crate::interaction::embed::COLOR_TRANSPARENT;
+use crate::translations::Lang;
 
 use super::parser::parse_message;
-use crate::cluster::ClusterState;
 
 /// Handle incoming [`Message`].
 ///
@@ -14,5 +21,41 @@ pub async fn handle_message(message: Message, state: Arc<ClusterState>) {
     let parsed = parse_message(&message);
     state.redis().set(&parsed).await.ok();
 
+    if is_an_old_command(&message) {
+        if let Err(error) = warn_about_old_command(&message, state.http()).await {
+            error!(error = ?error, "failed to warn user about question mark commands deprecation");
+        }
+    }
+
     info!("received message: {}", message.content) // Debug util real implementation
+}
+
+async fn warn_about_old_command(message: &Message, http: &Client) -> Result<()> {
+    let lang = Lang::fallback();
+    let embeds = [EmbedBuilder::new()
+        .title(lang.warning_deprecated_command_style_title())
+        .description(lang.warning_deprecated_command_style_description())
+        .color(COLOR_TRANSPARENT)
+        .build()];
+    http.create_message(message.channel_id)
+        .embeds(&embeds)?
+        .reply(message.id)
+        .exec()
+        .await
+        .context("warning about old commands with question  mark ?")?;
+    Ok(())
+}
+
+fn is_an_old_command(message: &Message) -> bool {
+    message.content.starts_with("?kick")
+        || message.content.starts_with("?userinfo")
+        || message.content.starts_with("?lock")
+        || message.content.starts_with("?unlock")
+        || message.content.starts_with("?raidmode")
+        || message.content.starts_with("?settings")
+        || message.content.starts_with("?captcha")
+        || message.content.starts_with("?ban")
+        || message.content.starts_with("?invite")
+        || message.content.starts_with("?clear")
+        || message.content.starts_with("?stats")
 }
