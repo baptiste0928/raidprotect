c39ec980c9f914e6c543d65315a0b6c906da64ee
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2022-09-10 15:57:22 +0200

    feat(interaction): captcha regenerate button (#151)

--- raidprotect/src/interaction/component/captcha/verify.rs
+++ raidprotect/src/interaction/component/captcha/verify.rs
@@ -63,7 +63,7 @@ impl CaptchaVerifyButton {
             }
         };
 
-        // Captcha has been regenerated too many times.
+        // Kick the user if the captcha has been regenerated too many times.
         if captcha.regenerate_count >= captcha::MAX_RETRY {
             let config = ctx.config(state).await?;
             let state_clone = state.clone();
@@ -79,7 +79,7 @@ impl CaptchaVerifyButton {
             return Ok(embed::captcha::regenerate_error(ctx.lang));
         }
 
-        // Generate the captcha code.
+        // Generate the captcha image.
         let code = random_human_code(captcha::DEFAULT_LENGTH);
 
         let code_clone = code.clone();
@@ -100,18 +100,31 @@ impl CaptchaVerifyButton {
             .image(ImageSource::attachment("captcha.png")?)
             .build();
 
-        let custom_id = CustomId::name("captcha-validate");
-        let component = Component::ActionRow(ActionRow {
-            components: vec![Component::Button(Button {
-                custom_id: Some(custom_id.to_string()),
-                label: Some(ctx.lang.captcha_image_button().to_string()),
-                style: ButtonStyle::Success,
+        let continue_id = CustomId::name("captcha-validate");
+        let mut components = vec![Component::Button(Button {
+            custom_id: Some(continue_id.to_string()),
+            label: Some(ctx.lang.captcha_image_button().to_string()),
+            style: ButtonStyle::Success,
+            disabled: false,
+            emoji: None,
+            url: None,
+        })];
+
+        // Add regenerate button if MAX_RETRY is not reached.
+        // The button will re-trigger the current interaction.
+        if captcha.regenerate_count < captcha::MAX_RETRY {
+            let regenerate_id = CustomId::name("captcha-verify");
+            components.push(Component::Button(Button {
+                custom_id: Some(regenerate_id.to_string()),
+                label: Some(ctx.lang.captcha_image_regenerate().to_string()),
+                style: ButtonStyle::Secondary,
                 disabled: false,
                 emoji: None,
                 url: None,
-            })],
-        });
+            }));
+        }
 
+        let component = Component::ActionRow(ActionRow { components });
         let attachment = Attachment {
             file: image,
             filename: "captcha.png".to_string(),
